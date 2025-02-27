434b1904b6d455f692cc73f88aec77a589b803f7
Gianluca Vinzens <gvinzens@inventage.com>
2025-02-27 10:30:53 +0100

    Remove unused scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,3 @@ set -a
 . ${SCRIPT_DIR}/.env
 env | grep 'REACT\|PUBLIC' && npx aoe_technology_radar-buildRadar && npx aoe_technology_radar-generateJson && npx aoe_technology_radar-createStaticFiles
 set +a
-
-#node ${SCRIPT_DIR}/patch-styles.js './build/'
-#node ${SCRIPT_DIR}/patch-translate.js './build/'
-#node ${SCRIPT_DIR}/patch-flag.js './build/'
-#node ${SCRIPT_DIR}/patch-description.js './build/'
